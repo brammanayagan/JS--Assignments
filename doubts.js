30b1a1620c889e20f872026a6c8da6a6fb9d471d
Brammanayagan S <sakthibrammanayagan@gmail.com>
2026-02-24 15:48:12 +0530

    day 04 assgn

---
+++ doubts.js
@@ -0,0 +1,3 @@
+const result = 7 + "10";
+
+console.log(typeof result);
---
+++ forLoop.js
@@ -0,0 +1,11 @@
+// day 01 array
+
+let array1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < array1.length; i++) {
+  if (array1[i] % 2 === 0) {
+    console.log("Even Numbers are: ", array1[i]);
+  }
+}
+
+// day 02 array
